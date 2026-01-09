3041b2e7dc02e07319171120d2b842a1d5990bce
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-09 10:43:09 +0530

    file is uploaded

---
+++ OPERATORS/Unary operator.java
@@ -0,0 +1,13 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Unary Operator");
+		int num1=45;
+		int num2=55;
+		
+		System.out.println("++num1 ="+(++num1));
+		System.out.println("--num2 ="+(--num2));
+		
+	}
+}
