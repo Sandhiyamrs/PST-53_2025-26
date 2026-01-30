e3bbf7dcc765d3c4d44f425eb0c736c6c4de8856
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-30 11:53:44 +0530

    Add files via upload

---
+++ Scanner/PST/num_factorial.java
@@ -0,0 +1,23 @@
+package PST;
+
+import java.util.Scanner;
+public class num_factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a number : " );
+		int num = sc.nextInt();
+		
+		long factorial = 1;
+		
+		for(int i = 1; i <= num; i++) {
+			factorial *= i;
+		
+		}
+			System.out.println("factorial of " + num + " is " + factorial);
+			
+			sc.close();
+	}
+
+}
