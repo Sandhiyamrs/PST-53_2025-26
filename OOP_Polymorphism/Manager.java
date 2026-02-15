e8d8a96a56ba319be3bf190f6b1dcbc0817e8329
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-02-15 23:26:35 +0530

    Add files via upload

---
+++ OOP_Polymorphism/Manager.java
@@ -0,0 +1,11 @@
+class Manager extends Employee {
+
+    public Manager(String name, double baseSalary) {
+        super(name, baseSalary);
+    }
+
+    @Override
+    double calculateSalary() {
+        return baseSalary + 5000;
+    }
+}
