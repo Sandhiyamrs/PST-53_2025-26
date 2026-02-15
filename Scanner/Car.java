f3ad171132aa17793bc679d446bb13063c8fabdc
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-02-15 19:24:31 +0530

    Add files via upload

---
+++ Scanner/Car.java
@@ -0,0 +1,11 @@
+class Car extends Vehicle {
+
+    public Car(String brand) {
+        super(brand);
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Car engine started");
+    }
+}
