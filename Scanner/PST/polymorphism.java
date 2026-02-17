da399a7df05e059f092d55a2e92ba059d68231e1
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-02-17 22:33:02 +0530

    Add files via upload

---
+++ Scanner/PST/polymorphism.java
@@ -0,0 +1,18 @@
+package PST;
+
+public class polymorphism{
+	public static void main(String[] args) 
+	{
+     System.out.println(method());
+
+	}
+	
+	
+	public static int method()
+	{
+		int a=2515;
+				return a;
+		
+	}
+   
+}
