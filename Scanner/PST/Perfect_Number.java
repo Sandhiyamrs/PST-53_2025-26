10bd95a4427a96ac177a8d0465d25e8bbd0ae3b8
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-02-17 22:31:43 +0530

    Add files via upload

---
+++ Scanner/PST/Perfect_Number.java
@@ -0,0 +1,20 @@
+package PST;
+
+import java.util.Scanner;
+public class Perfect_Number {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a Number : ");
+		int num = sc.nextInt();
+		
+		int sum = 0;
+		int temp =num;
+		
+		for(int i = 0; i <= temp/2;i++) {
+			
+		}
+	}
+
+}
