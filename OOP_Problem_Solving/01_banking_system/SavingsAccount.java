c6863c5146ba704da87cbdc3630ad4a6f12cddc5
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-02-18 22:50:27 +0530

    Add files via upload

---
+++ OOP_Problem_Solving/01_banking_system/SavingsAccount.java
@@ -0,0 +1,10 @@
+class SavingsAccount extends BankAccount {
+    public SavingsAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
+    @Override
+    double calculateInterest() {
+        return balance * 0.04;
+    }
+}
