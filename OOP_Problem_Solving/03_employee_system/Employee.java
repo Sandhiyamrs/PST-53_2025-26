2b187cc4d54f03c355626f10035484bdeb0daf4c
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-02-18 23:05:09 +0530

    Add files via upload

---
+++ OOP_Problem_Solving/03_employee_system/Employee.java
@@ -0,0 +1,11 @@
+abstract class Employee {
+    protected String name;
+    protected double baseSalary;
+
+    public Employee(String name, double baseSalary) {
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    abstract double calculateSalary();
+}
