f031731ed9c4dd593aedc60e806cf364467458b9
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-02-18 22:51:51 +0530

    Add files via upload

---
+++ OOP_Problem_Solving/02_vehicle_system/Bike.java
@@ -0,0 +1,10 @@
+class Bike extends Vehicle {
+    public Bike(String brand) {
+        super(brand);
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Bike engine started");
+    }
+}
