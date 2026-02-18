40eab4c56347f7c3b796c9697b7087a97a2af4ce
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-02-18 22:46:28 +0530

    Add files via upload

---
+++ OOP_Problem_Solving/01_banking_system/BankAccount.java
@@ -0,0 +1,19 @@
+abstract class BankAccount {
+    protected String accountNumber;
+    protected double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    abstract double calculateInterest();
+}
